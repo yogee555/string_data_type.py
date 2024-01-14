e280886f89b1f267be5be93773dfab736a23d73a
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-01-14 10:06:09 +0530

    Add files via upload

---
+++ string_data_type.py
@@ -0,0 +1,49 @@
+# string data types & its function
+a = "python"
+print(a[0])
+
+a = "python"
+print(a[1])
+
+a = "python"
+print(a[2])
+
+a = "python"
+print(a[3])
+
+a = "python"
+print(a[4])
+
+a = "python"
+print(a[5])
+
+a = "python"
+print(a[-1])
+
+a = "python"
+print(a[-2])
+
+a = "python"
+print(a[-3])
+
+a = "python"
+print(a[-4])
+
+a = "python"
+print(a[-5])
+
+a = "python"
+print(a[-6])
+
+a = "python"
+print(a[0:3])
+
+a = "python"
+print(a[2:3])
+
+a = "python"
+print(a[0:-2])
+
+a = "python"
+print(a[0:5:2])
+
